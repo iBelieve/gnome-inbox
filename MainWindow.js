f6f21c21ce02584491a4799ce4a201b0a4f0e4be
Michael Spencer <sonrisesoftware@gmail.com>
2016-12-17 22:40:00 -0600

    Split the webview into a separate file and add replies from the webview

--- MainWindow.js
+++ MainWindow.js
@@ -1,38 +1,6 @@
 const Gtk = require('Gtk'),
       WebKit = require('WebKit2'),
-      fs = require('fs')
-
-const STYLESHEET = `
-  /* A container holding the compose FAB + menu */
-  .nPQzwd.iP {
-    display: none;
-  }
-
-  /* Container holding hangout chat windows */
-  #OPOhoe {
-    display: none;
-  }
-
-  /* Navigation bar */
-  .Ut6z6b {
-    display: none;
-  }
-
-  /* Remove padding needed for the navbar */
-  .cM.bz {
-    padding-top: 0 !important;
-  }
-
-  /* Right navbar buttons */
-  .rs.k2 {
-    display: none;
-  }
-
-  /* An IFrame causing a weird dot in the upper left corner */
-  #gtn-roster-iframe-id {
-    display: none;
-  }
-`
+      WebView = require('./WebView')
 
 class MainWindow extends Gtk.ApplicationWindow {
   constructor({ application } = {}) {
@@ -57,7 +25,7 @@ class MainWindow extends Gtk.ApplicationWindow {
     const composeIcon = new Gtk.Image({ iconName: 'document-edit-symbolic',
                                         iconSize: Gtk.IconSize.SMALL_TOOLBAR })
     const composeButton = new Gtk.Button({ label: 'Compose', image: composeIcon })
-    composeButton.on('clicked', () => this.click('.y.hC'))
+    composeButton.on('clicked', () => this.webView.click('.y.hC'))
 
     // headerStart.attach(this.widgetOpen.button, 0, 0, 1, 1)
     // headerStart.attach(composeButton, 1, 0, 1, 1)
@@ -68,37 +36,9 @@ class MainWindow extends Gtk.ApplicationWindow {
     return header
   }
 
-  click(selector) {
-    const code = `document.querySelectorAll('${selector}').item(0).click()`
-
-    this.webView.runJavaScript(code, null, (webView, result) => {
-      this.webView.runJavaScriptFinish(result)
-    })
-  }
-
-  setUpContentManager() {
-    this.contentManager = new WebKit.UserContentManager()
-
-    const stylesheet = new WebKit.UserStyleSheet(STYLESHEET,
-                                                 WebKit.UserContentInjectedFrames.ALL_FRAMES,
-                                                 WebKit.UserStyleLevel.USER, null, null)
-
-    this.contentManager.addStyleSheet(stylesheet)
-  }
-
-  setUpWebContext() {
-    this.webContext = this.webView.getContext()
-    this.webContext.getCookieManager().setPersistentStorage(`${__dirname}/cookies.txt`,
-                                                            WebKit.CookiePersistentStorage.TEXT)
-  }
-
   getWebView() {
-    this.setUpContentManager()
-
-    this.webView = new WebKit.WebView({ userContentManager: this.contentManager })
-    this.webView.loadUri('https://inbox.google.com')
-
-    this.setUpWebContext()
+    this.webView = new WebView({ userContentManager: new WebKit.UserContentManager() })
+    this.webView.loadInbox()
 
     return this.webView
   }
---
+++ WebView.js
@@ -0,0 +1,145 @@
+const WebKit = require('WebKit2')
+
+const CHANNEL = 'jsgtk'
+
+const STYLESHEET = `
+  /* A container holding the compose FAB + menu */
+  .nPQzwd.iP {
+    display: none;
+  }
+
+  /* Container holding hangout chat windows */
+  #OPOhoe {
+    display: none;
+  }
+
+  /* Navigation bar */
+  .Ut6z6b {
+    display: none;
+  }
+
+  /* Remove padding needed for the navbar */
+  .cM.bz {
+    padding-top: 0 !important;
+  }
+
+  /* Right navbar buttons */
+  .rs.k2 {
+    display: none;
+  }
+
+  /* An IFrame causing a weird dot in the upper left corner */
+  #gtn-roster-iframe-id {
+    display: none;
+  }
+`
+
+const SCRIPTS = `
+  function injectClick(selector) {
+    document.querySelectorAll(selector).item(0).click()
+  }
+
+  function getUserAvatar() {
+    try {
+      var elem = document.querySelectorAll('.gb_8a.gbii').item(0)
+      var css = window.getComputedStyle(elem)
+      var url = css.backgroundImage.match(${/url\((.*)\)/})[1]
+
+      reply('getUserAvatar', url)
+    } catch (error) {
+      reply('getUserAvatar', 'Error: ' + error.message)
+    }
+  }
+
+  function reply(method, data) {
+    // Webkit will be notified about a request change
+    location.href = '${CHANNEL}:' + encodeURIComponent(JSON.stringify({ method: method, data: data }))
+  }
+`
+
+class WebView extends WebKit.WebView {
+  constructor({ userContentManager }) {
+    super({ userContentManager })
+
+    this.on('load-changed', this.onLoadChanged.bind(this))
+    this.on('decide-policy', this.onDecidePolicy.bind(this))
+
+    this.setUpContentManager()
+    this.setUpWebContext()
+  }
+
+  setUpContentManager() {
+    const contentManager = this.getUserContentManager()
+
+    const stylesheet = new WebKit.UserStyleSheet(STYLESHEET,
+                                                 WebKit.UserContentInjectedFrames.ALL_FRAMES,
+                                                 WebKit.UserStyleLevel.USER, null, null)
+
+    const userScript = new WebKit.UserScript(SCRIPTS, WebKit.UserContentInjectedFrames.ALL_FRAMES,
+                                             WebKit.UserStyleLevel.USER, null, null)
+
+    contentManager.addStyleSheet(stylesheet)
+    contentManager.addScript(userScript)
+  }
+
+  setUpWebContext() {
+    const context = this.getContext()
+
+    context.getCookieManager().setPersistentStorage(`${__dirname}/cookies.txt`,
+                                                    WebKit.CookiePersistentStorage.TEXT)
+  }
+
+  loadInbox() {
+    this.loadUri('https://inbox.google.com')
+  }
+
+  exec(code) {
+    console.log(`Executing: ${code}`)
+    this.runJavaScript(code, null, (webView, result) => {
+      this.runJavaScriptFinish(result)
+    })
+  }
+
+  click(selector) {
+    this.exec(`injectClick('${selector}')`)
+  }
+
+  getUserAvatar() {
+    this.exec('getUserAvatar()')
+  }
+
+  onReply(method, data) {
+    switch (method) {
+    case 'getUserAvatar':
+      console.log(data)
+      break
+    }
+  }
+
+  onDecidePolicy(webView, policy, type) {
+    switch(type) {
+    case WebKit.PolicyDecisionType.NAVIGATION_ACTION:
+      const uri = policy.getRequest().getUri()
+
+      if (uri.indexOf(CHANNEL + ':') === 0) {
+        const response = JSON.parse(decodeURIComponent(uri.slice(CHANNEL.length + 1)))
+        policy.ignore()
+        this.onReply(response.method, response.data)
+      }
+      break
+    }
+  }
+
+  onLoadChanged(webView, event, data) {
+    switch (event) {
+    case WebKit.LoadEvent.FINISHED:
+      if (this.uri === 'https://inbox.google.com/') {
+        console.log('Fully loaded!')
+        this.getUserAvatar()
+      }
+      break
+    }
+  }
+}
+
+module.exports = WebView
